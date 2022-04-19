9174c139ed64338f994b9b373b5ac36c0751df87
Folareen <sakawahab03@gmail.com>
2022-04-19 15:45:41 +0100

    done with mobile view header and navigation

--- home.js
+++ home.js
@@ -1,1 +1,9 @@
-const 
+const NAV_BUTTON = document.querySelector('.hamburger');
+const NAV = document.querySelector('nav');
+
+NAV_BUTTON.addEventListener('click', openNav);
+
+function openNav(){
+    NAV_BUTTON.classList.toggle('open-nav')
+    NAV.classList.toggle('open');
+}
