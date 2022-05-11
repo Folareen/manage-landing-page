0a0cd3f394c055b5a0e42139cf67a960c0f7d1cd
Folareen <sakawahab03@gmail.com>
2022-05-11 12:24:59 +0100

    adjusted testimonies slider

--- home.js
+++ home.js
@@ -14,26 +14,41 @@ const TESTIMONIES_CONTAINER = document.querySelector('.testimonies');
 let testimoniesPosition = document.querySelector('.testimonies__content').getBoundingClientRect().width
 
 let curr ;
-function paginate(page){
+function paginate(page, page2){
     Array.from(document.querySelectorAll('.pagination > span')).forEach((item) => item.classList.remove('current'));
-    Array.from(document.querySelectorAll('.pagination > span'))[page].classList.add('current');
+
+    if(page2 != undefined){
+        Array.from(document.querySelectorAll('.pagination > span'))[page].classList.add('current');
+        Array.from(document.querySelectorAll('.pagination > span'))[page2].classList.add('current');
+    }else{
+        Array.from(document.querySelectorAll('.pagination > span'))[page].classList.add('current');
+    }
 }
 paginate(0);
 
+if(TESTIMONIES_CONTAINER.getBoundingClientRect().width > 1534 ){
+    paginate(0, 1);
+}
+
 function moveTranslate(){
-    if(TESTIMONIES_CONTAINER.getBoundingClientRect().width > 1800 ){
+    if(TESTIMONIES_CONTAINER.getBoundingClientRect().width > 1534 ){
+        paginate(0, 1);
+
 
         setTimeout(
             () => {
                 TESTIMONIES_CONTAINER.style.transform = `translateX(-${testimoniesPosition + 20}px)`;
+                paginate(1, 2)
         
                 setTimeout(
                     () => {
                         TESTIMONIES_CONTAINER.style.transform = `translateX(-${testimoniesPosition + testimoniesPosition + 40}px)`;
+                        paginate(2, 3)
 
                         setTimeout(
                             () => {
                                 TESTIMONIES_CONTAINER.style.transform = `translateX(0px)`;
+                                paginate(0, 1)
 
                         
                             }, 4000
