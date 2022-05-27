13ec386b57ea155d1f9793297973ed56abc0ee27
Folareen <sakawahab03@gmail.com>
2022-05-27 03:45:59 +0100

    added slider control

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
 const NAV_BUTTON = document.querySelector('.hamburger');
 const NAV = document.querySelector('nav');
+const TESTIMONIES_CONTAINER = document.querySelector('.testimonies');
+const SLIDER_CONTROL = Array.from(document.querySelectorAll('.pagination > span'));
 
 NAV_BUTTON.addEventListener('click', openNav);
 
@@ -9,19 +11,26 @@ function openNav(){
     document.body.classList.toggle('nav-fixed')
 }
 
-const TESTIMONIES_CONTAINER = document.querySelector('.testimonies');
-
 let testimoniesPosition = document.querySelector('.testimonies__content').getBoundingClientRect().width
+
+SLIDER_CONTROL.forEach(
+    (control, index)=>{
+        control.addEventListener('click', ()=>{
+            TESTIMONIES_CONTAINER.style.transform = `translateX(-${(testimoniesPosition * index) + (20 * index) }px)`;
+            paginate(index)
+        })
+    }
+)
 
 let curr ;
 function paginate(page, page2){
-    Array.from(document.querySelectorAll('.pagination > span')).forEach((item) => item.classList.remove('current'));
+    SLIDER_CONTROL.forEach((item) => item.classList.remove('current'));
 
     if(page2 != undefined){
-        Array.from(document.querySelectorAll('.pagination > span'))[page].classList.add('current');
-        Array.from(document.querySelectorAll('.pagination > span'))[page2].classList.add('current');
+        SLIDER_CONTROL[page].classList.add('current');
+        SLIDER_CONTROL[page2].classList.add('current');
     }else{
-        Array.from(document.querySelectorAll('.pagination > span'))[page].classList.add('current');
+        SLIDER_CONTROL[page].classList.add('current');
     }
 }
 paginate(0);
@@ -78,14 +87,12 @@ function moveTranslate(){
 
 
     else{
-
         (async ()=>{
             await(        
                 transform(
                     ()=>{
                         TESTIMONIES_CONTAINER.style.transform = `translateX(-${testimoniesPosition + 20}px)`;
                         paginate(1)
-                        console.log(1);
                     }, 4000
                 )
             )
@@ -95,7 +102,6 @@ function moveTranslate(){
                     ()=>{
                         TESTIMONIES_CONTAINER.style.transform = `translateX(-${testimoniesPosition + testimoniesPosition + 40}px)`;
                         paginate(2)
-                        console.log(2);
                     }, 4000
                 )
             )
@@ -105,7 +111,6 @@ function moveTranslate(){
                     ()=>{
                         TESTIMONIES_CONTAINER.style.transform = `translateX(-${testimoniesPosition + testimoniesPosition + testimoniesPosition + 60}px)`;
                         paginate(3)
-                        console.log(3);
                     }, 4000
                 )
             )
@@ -115,7 +120,6 @@ function moveTranslate(){
                     ()=>{
                         TESTIMONIES_CONTAINER.style.transform = `translateX(-${testimoniesPosition + testimoniesPosition + testimoniesPosition + 60}px)`;
                         paginate(3)
-                        console.log(3);
                     }, 4000
                 )
             )
@@ -125,7 +129,6 @@ function moveTranslate(){
                     ()=>{
                         TESTIMONIES_CONTAINER.style.transform = `translateX(0px)`;
                         paginate(0)
-                        console.log(0);
                     }, 4000
                 )
             )
@@ -134,7 +137,6 @@ function moveTranslate(){
     }
 
 }
-
 
 setTimeout(() => {
     moveTranslate();
